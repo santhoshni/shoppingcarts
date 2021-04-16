d1492e2d194be45459e036893609d59d271fce94
Santhoshni <dsanthoshni@gmail.com>
2021-04-16 17:29:59 -0500

    Added Java and Html pages

---
+++ src/main/java/com/santhoshni/shoppingcarts/Controllers/HomeController.java
@@ -0,0 +1,15 @@
+package com.santhoshni.shoppingcarts.Controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    //@GetMapping("/someRandomPage")
+    @GetMapping("/")
+    public String home() {
+        return "home";
+    }
+    
+}
