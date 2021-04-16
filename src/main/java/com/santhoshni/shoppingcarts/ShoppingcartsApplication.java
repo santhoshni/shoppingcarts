44e4602b5dc1425dbf941ccfc168a5fa375730d5
Santhoshni <dsanthoshni@gmail.com>
2021-04-16 15:43:57 -0500

    initial commit

---
+++ src/main/java/com/santhoshni/shoppingcarts/ShoppingcartsApplication.java
@@ -0,0 +1,13 @@
+package com.santhoshni.shoppingcarts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingcartsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppingcartsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/santhoshni/shoppingcarts/ShoppingcartsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.santhoshni.shoppingcarts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingcartsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
